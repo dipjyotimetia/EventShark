1191c2bc77b9652abe8435dd3babf5a928bfc98d
Dipjyoti Metia <dipjyotimetia@gmail.com>
2023-09-17 15:48:24 +1000

    update

--- docker/create_topics.sh
+++ docker/create_topics.sh
@@ -1,23 +1,45 @@
 #!/bin/sh
 
-# Install curl
-apt-get update && apt-get install -y curl
+# Function to check if a Kafka topic exists
+topic_exists() {
+  local topic_name="$1"
+  local brokers="$2"
+  rpk --brokers "$brokers" topic describe "$topic_name" >/dev/null 2>&1
+  return $?
+}
 
-# Connect to kafka and create topics with schema
-rpk --brokers kafka:29092 topic create expense-topic
-rpk --brokers kafka:29092 topic create payment-topic
-rpk --brokers kafka:29092 topic create transaction-topic
+# Install curl if not already installed
+if ! command -v curl >/dev/null 2>&1; then
+  apt-get update && apt-get install -y curl
+fi
 
-# Add schema to the Schema Registry
-curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
-    --data @/expense.json http://kafka:8081/subjects/expense-topic-value/versions
+# Define Kafka broker address
+KAFKA_BROKERS="kafka:29092"
 
-curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
-    --data @/payment.json http://kafka:8081/subjects/payment-topic-value/versions
+# Define Schema Registry URL
+SCHEMA_REGISTRY_URL="http://kafka:8081"
 
-curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
-    --data @/transaction.json http://kafka:8081/subjects/transaction-topic-value/versions
+# Define schemas and topics
+schemas=("expense.json" "payment.json" "transaction.json")
+topics=("expense-topic" "payment-topic" "transaction-topic")
 
-rpk --brokers kafka:29092 topic info expense-topic
-rpk --brokers kafka:29092 topic info payment-topic
-rpk --brokers kafka:29092 topic info transaction-topic
+# Create topics and register schemas
+for ((i=0; i<${#topics[@]}; i++)); do
+  topic="${topics[i]}"
+  schema_file="${schemas[i]}"
+
+  # Check if the topic exists, create it if not
+  if ! topic_exists "$topic" "$KAFKA_BROKERS"; then
+    rpk --brokers "$KAFKA_BROKERS" topic create "$topic"
+  fi
+
+  # Register schema with the Schema Registry
+  curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
+    --data "@/$schema_file" "$SCHEMA_REGISTRY_URL/subjects/${topic}-value/versions"
+
+  # Display topic information
+  rpk --brokers "$KAFKA_BROKERS" topic info "$topic"
+done
+
+# Clean up
+apt-get remove -y curl
